0c11d417de005784d4f658d0a34126821d4ccb48
Alyson Girao <alysongirao@gmail.com>
2019-12-15 21:49:03 -0300

    Adicionada configuracao para o renderer

--- computerstore/settings.py
+++ computerstore/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config, Csv
 from dj_database_url import parse as dburl
+import rest_framework
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': 
+    config('DEFAULT_RENDERER_CLASSES', cast=tuple, default=('rest_framework.renderers.BrowsableAPIRenderer',)),
+}
