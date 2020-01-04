0188dbe145e072785cebec612169e0623cb24d5b
Alyson Girao <alysongirao@gmail.com>
2020-01-03 22:23:07 -0300

    Adicionado testes para a criacao de uma ordem e validacao de processador e placa mae

--- computerstore/computers/serializers.py
+++ computerstore/computers/serializers.py
@@ -5,47 +5,41 @@ from computerstore.computers.models import Order, Processor, Memory, GraphicsCar
 class ProcessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Processor
-        fields = ['name']
+        fields = ['id','name']
 
 
 class MemorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Memory
-        fields = ['name']
+        fields = ['id','name']
 
 
 class GraphicsCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = GraphicsCard
-        fields = ['name']
+        fields = ['id','name']
 
 
 class MotherBoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = MotherBoard
-        fields = ['name']
+        fields = ['id','name']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    client = serializers.CharField(max_length=100)
-    processor = serializers.SlugRelatedField(slug_field='name', queryset=Processor.objects.all())
-    memory = serializers.SlugRelatedField(slug_field='name', queryset=Memory.objects.all(), many=True)
-    gpu = serializers.SlugRelatedField(slug_field='name', queryset=GraphicsCard.objects.all(), required=False)
-    motherboard = serializers.SlugRelatedField(slug_field='name', queryset=MotherBoard.objects.all())
 
     class Meta:
         model = Order
-        fields = ['client', 'processor', 'memory', 'gpu', 'motherboard']
+        fields = ['client', 'processor', 'memory', 'gpu', 'motherboard', 'memory']
 
     def validate(self, data):
-        data = self.get_initial()
-        processor_data = data['processor']
-        motherboard_data = data['motherboard']
-        processor = Processor.objects.get(name__exact=processor_data)
-        motherboard = MotherBoard.objects.get(name__exact=motherboard_data)
+        processor = data['processor']
+        motherboard = data['motherboard']
 
         if processor.brand != motherboard.brand:
-            raise serializers.ValidationError("Processador e placa mão não são compativeis")
+            raise serializers.ValidationError('Processador e placa mão não são compativeis')
 
         if not motherboard.integrated_graphics and 'gpu' not in data.keys():
             raise serializers.ValidationError('Placa mãe precisa de uma placa de vídeo!')
+
+        return data
