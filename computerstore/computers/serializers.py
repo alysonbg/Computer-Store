b9041d1b30b85a9168cb30df7ed0ee35fb149431
Alyson Girao <alysongirao@gmail.com>
2020-01-04 12:08:28 -0300

    Finalizacao do metodo to_representation

--- computerstore/computers/serializers.py
+++ computerstore/computers/serializers.py
@@ -5,32 +5,32 @@ from computerstore.computers.models import Order, Processor, Memory, GraphicsCar
 class ProcessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Processor
-        fields = ['id','name']
+        fields = ['name', 'brand']
 
 
 class MemorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Memory
-        fields = ['id','name']
+        fields = ['name', 'size']
 
 
 class GraphicsCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = GraphicsCard
-        fields = ['id','name']
+        fields = ['name']
 
 
 class MotherBoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = MotherBoard
-        fields = ['id','name']
+        fields = ['name', 'brand', 'ram_slots', 'max_ram', 'integrated_graphics']
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['client', 'processor', 'memory', 'gpu', 'motherboard', 'memory']
+        fields = ['id','client', 'processor', 'memory', 'gpu', 'motherboard']
 
     def validate(self, data):
         processor = data['processor']
@@ -42,4 +42,12 @@ class OrderSerializer(serializers.ModelSerializer):
         if not motherboard.integrated_graphics and 'gpu' not in data.keys():
             raise serializers.ValidationError('Placa mãe precisa de uma placa de vídeo!')
 
+        return data
+
+    def to_representation(self, instance):
+        data = super(OrderSerializer, self).to_representation(instance)
+        data['processor'] = ProcessorSerializer(instance.processor).data
+        data['memory'] = [MemorySerializer(memory).data for memory in Memory.objects.filter(pk__in=data['memory'])]
+        data['gpu'] = GraphicsCardSerializer(instance.gpu).data
+        data['motherboard'] = MotherBoardSerializer(instance.motherboard).data
         return data
